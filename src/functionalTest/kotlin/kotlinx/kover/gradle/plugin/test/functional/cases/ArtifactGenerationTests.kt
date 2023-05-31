ea29b9f00e593dd882a15952503ed7f6d3f3deb5
Sergey Shanshin <sergey.shanshin@jetbrains.com>
2023-05-31 21:41:12 +0300

    Removed Kover artifact resolution during configuration time
    The task tree is built at the configuration stage, so getting artifacts from dependencies can lead to premature task launches, deadlocks, and performance degradation.
    To eliminate resolution, it was necessary to use the Provider obtained from the corresponding file collection.
    
    PR #386

--- src/functionalTest/kotlin/kotlinx/kover/gradle/plugin/test/functional/cases/ArtifactGenerationTests.kt
+++ src/functionalTest/kotlin/kotlinx/kover/gradle/plugin/test/functional/cases/ArtifactGenerationTests.kt
@@ -3,8 +3,11 @@
  */
 package kotlinx.kover.gradle.plugin.test.functional.cases
 
+import kotlinx.kover.gradle.plugin.test.functional.framework.checker.CheckerContext
 import kotlinx.kover.gradle.plugin.test.functional.framework.configurator.BuildConfigurator
+import kotlinx.kover.gradle.plugin.test.functional.framework.starter.ExamplesTest
 import kotlinx.kover.gradle.plugin.test.functional.framework.starter.GeneratedTest
+import kotlin.test.*
 
 internal class ArtifactGenerationTests {
 
@@ -20,5 +23,25 @@ internal class ArtifactGenerationTests {
             taskNotCalled("test")
             taskNotCalled("koverGenerateArtifact")
         }
+    }
+
+    /**
+     * Check that Kover artifact files are not resolved during the task dependency tree construction process.
+     *
+     * The task tree is built at the configuration stage, so getting artifacts from dependencies can lead to premature task launches, deadlocks, and performance degradation.
+     *
+     * If a resolution is detected during configuration, the message "Configuration 'koverExternalArtifactsRelease' was resolved during configuration time" is printed.
+     * This message may be changed in future versions, so it's worth double-checking for this error.
+     */
+    @ExamplesTest("android/multiproject", [":app:koverXmlReportRelease"])
+    fun CheckerContext.testResolveConfigurationInExecuteTime() {
+        assertFalse(
+            output.contains("Configuration 'koverExternalArtifactsRelease' was resolved during configuration time"),
+            "Kover Configuration was resolved during configuration time"
+        )
+        assertFalse(
+            output.contains("This is a build performance and scalability issue"),
+            "Some Configuration was resolved during configuration time, perhaps this is the Kover Configuration"
+        )
     }
 }
