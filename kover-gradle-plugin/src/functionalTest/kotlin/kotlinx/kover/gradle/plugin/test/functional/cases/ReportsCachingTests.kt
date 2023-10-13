6f95f5d19c37403c3da378a2e518c02852667a06
Sergey.Shanshin <sergey.shanshin@jetbrains.com>
2023-10-13 14:12:54 +0200

    Fixed adding data to an already existing binary report file
    If the build cache is not used and the build directory is not cleared before the test run, then the coverage data from previous test runs are merged with the data from the recent run.
    In this case, the report may include coverage from those tests that were removed in the latest version of the code.
    
    To solve this, it is necessary to delete the binary report file before each run of the test task.
    
    Fixes #489

--- kover-gradle-plugin/src/functionalTest/kotlin/kotlinx/kover/gradle/plugin/test/functional/cases/ReportsCachingTests.kt
+++ kover-gradle-plugin/src/functionalTest/kotlin/kotlinx/kover/gradle/plugin/test/functional/cases/ReportsCachingTests.kt
@@ -3,6 +3,7 @@
  */
 package kotlinx.kover.gradle.plugin.test.functional.cases
 
+import kotlinx.kover.gradle.plugin.commons.CoverageToolVendor
 import kotlinx.kover.gradle.plugin.test.functional.framework.configurator.*
 import kotlinx.kover.gradle.plugin.test.functional.framework.starter.*
 
@@ -29,55 +30,80 @@ internal class ReportsCachingTests {
         addProjectWithKover {
             sourcesFrom("simple")
         }
-        reportAndCheck("SUCCESS", true)
+        reportAndCheck("SUCCESS", cached = true)
 
         run("clean", "--build-cache") {
             checkDefaultBinReport(false)
             checkDefaultReports(false)
         }
-        reportAndCheck("FROM-CACHE", true)
+        reportAndCheck("FROM-CACHE", cached = true)
     }
 
-    @SlicedGeneratedTest(allTools = true)
-    fun BuildConfigurator.testOuOfDateOnSources() {
+    @GeneratedTest(tool = CoverageToolVendor.KOVER)
+    fun BuildConfigurator.testKoverOutOfDateOnSources() {
         useLocalCache()
 
         addProjectWithKover {
             sourcesFrom("simple")
         }
-        reportAndCheck("SUCCESS", true)
+        reportAndCheck("SUCCESS", cached = true)
 
         edit("src/main/kotlin/Sources.kt") {
             "$it\n class Additional"
         }
         // tasks must be restarted after the source code is edited
-        reportAndCheck("SUCCESS", true)
+        reportAndCheck("SUCCESS", cached = true)
 
         edit("src/test/kotlin/TestClass.kt") {
             "$it\n class AdditionalTest"
         }
 
-        // tasks must be restarted after tests are edited
-        reportAndCheck("SUCCESS", true)
+        // test task must be restarted after test class is edited, but reports is up-to-date because no sources changed
+        reportAndCheck("SUCCESS", "UP-TO-DATE",true)
     }
 
-    @SlicedGeneratedTest(allTools = true)
+    @GeneratedTest(tool = CoverageToolVendor.JACOCO)
+    fun BuildConfigurator.testJaCoCoOutOfDateOnSources() {
+        useLocalCache()
+
+        addProjectWithKover {
+            sourcesFrom("simple")
+        }
+        reportAndCheck("SUCCESS", cached = true)
+
+        edit("src/main/kotlin/Sources.kt") {
+            "$it\n class Additional"
+        }
+        // tasks must be restarted after the source code is edited
+        reportAndCheck("SUCCESS", cached = true)
+
+        edit("src/test/kotlin/TestClass.kt") {
+            "$it\n class AdditionalTest"
+        }
+
+        // test task must be restarted after test class is edited,
+        // reports is not up-to-date because JaCoCo .exec binary report contains instrumentation time
+        reportAndCheck("SUCCESS", "SUCCESS",true)
+    }
+
+    @GeneratedTest(tool = CoverageToolVendor.KOVER)
     fun BuildConfigurator.testProjectReportCaching() {
         useLocalCache()
 
         addProjectWithKover {
             sourcesFrom("simple")
         }
-        reportAndCheck("SUCCESS", true)
+        reportAndCheck("SUCCESS", cached = true)
         run("clean", "--build-cache") {
             checkDefaultBinReport(false)
             checkDefaultReports(false)
         }
-        reportAndCheck("FROM-CACHE", true)
+        reportAndCheck("FROM-CACHE", cached = true)
     }
 
 
-    private fun BuildConfigurator.reportAndCheck(outcome: String, cached: Boolean = false) {
+
+    private fun BuildConfigurator.reportAndCheck(testOutcome: String, reportsOutcome: String = testOutcome, cached: Boolean = false) {
         val args = if (cached) {
             arrayOf("koverXmlReport", "koverHtmlReport", "--build-cache")
         } else {
@@ -86,9 +112,9 @@ internal class ReportsCachingTests {
         run(*args) {
             checkDefaultBinReport()
             checkDefaultReports()
-            checkOutcome("test", outcome)
-            checkOutcome("koverXmlReport", outcome)
-            checkOutcome("koverHtmlReport", outcome)
+            checkOutcome("test", testOutcome)
+            checkOutcome("koverXmlReport", reportsOutcome)
+            checkOutcome("koverHtmlReport", reportsOutcome)
         }
     }
 }
