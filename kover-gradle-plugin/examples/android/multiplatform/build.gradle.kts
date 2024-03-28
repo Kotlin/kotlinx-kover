3e1fe4016159f5e90b4665235f080fe72da3fb15
Sergey.Shanshin <sergey.shanshin@jetbrains.com>
2024-03-28 14:56:22 +0100

    Release 0.8.0-Beta2

--- kover-gradle-plugin/examples/android/multiplatform/build.gradle.kts
+++ kover-gradle-plugin/examples/android/multiplatform/build.gradle.kts
@@ -2,7 +2,7 @@ plugins {
     id("com.android.application") version "7.4.0" apply false
     id("com.android.library") version "7.4.0" apply false
     kotlin("multiplatform") version ("1.8.20") apply false
-    id("org.jetbrains.kotlinx.kover") version "0.8.0-Beta"
+    id("org.jetbrains.kotlinx.kover") version "0.8.0-Beta2"
 }
 
 dependencies {
