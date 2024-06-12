3bee2a89921d7f82b1ec1714fe71563593629b4f
Sergey Shanshin <sergey.shanshin@jetbrains.com>
2024-06-12 15:08:27 +0300

    Fixed coverage evaluation for enum in K2
    Fixes #621
    PR #631

---
+++ kover-gradle-plugin/src/functionalTest/templates/builds/k2-counters/build.gradle.kts
@@ -0,0 +1,8 @@
+plugins {
+    kotlin("jvm") version "2.0.0"
+    id("org.jetbrains.kotlinx.kover")
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+}
---
+++ kover-gradle-plugin/src/functionalTest/templates/builds/k2-counters/src/test/kotlin/TestClass.kt
@@ -0,0 +1,11 @@
+package org.jetbrains.serialuser
+
+import org.jetbrains.*
+import kotlin.test.Test
+
+class TestClass {
+    @Test
+    fun testBranches() {
+        println(K2Enum.A)
+    }
+}
