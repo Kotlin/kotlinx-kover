c482b09e1c6e05e8839706826edf306bc2a299d1
Sergey.Shanshin <sergey.shanshin@jetbrains.com>
2024-06-19 18:33:24 +0200

    ~fix

--- kover-gradle-plugin/src/functionalTest/templates/builds/counters/src/main/java/TryWithResources.java
+++ kover-gradle-plugin/src/functionalTest/templates/builds/counters/src/main/java/TryWithResources.java
@@ -5,8 +5,12 @@ public final class TryWithResources {
     private TryWithResources() {}
 
     public static void test() throws Exception {
-        try (AutoCloseable ignored = () -> System.out.println("closed")) {
+        try (AutoCloseable ignored = generate()) {
             System.out.println("action");
         }
+    }
+
+    private static AutoCloseable generate() {
+        return () -> System.out.println("closed");
     }
 }
