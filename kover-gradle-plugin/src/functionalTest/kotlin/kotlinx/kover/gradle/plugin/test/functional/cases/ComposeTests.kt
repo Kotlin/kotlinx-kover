8cee8cba8e4265a4a5e9266428574960300473ca
Sergey Shanshin <sergey.shanshin@jetbrains.com>
2024-06-27 18:52:11 +0300

    Added compose tests
    Fixes #601
    
    PR #640

---
+++ kover-gradle-plugin/src/functionalTest/kotlin/kotlinx/kover/gradle/plugin/test/functional/cases/ComposeTests.kt
@@ -0,0 +1,26 @@
+/*
+ * Copyright 2017-2024 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license.
+ */
+
+package kotlinx.kover.gradle.plugin.test.functional.cases
+
+import kotlinx.kover.gradle.plugin.test.functional.framework.checker.CheckerContext
+import kotlinx.kover.gradle.plugin.test.functional.framework.starter.TemplateTest
+
+internal class ComposeTests {
+    @TemplateTest("android-compose", [":app:koverXmlReportDebug", ":app:koverHtmlReportDebug"])
+    fun CheckerContext.testCompose() {
+        subproject("app") {
+            xmlReport("debug") {
+                methodCounter("org.jetbrains.composetests.ComposeFunctionsKt", "Simple").assertFullyCovered()
+                methodCounter("org.jetbrains.composetests.ComposeFunctionsKt", "Simple", "BRANCH").assertFullyCovered()
+
+                methodCounter("org.jetbrains.composetests.ComposeFunctionsKt", "WithParam").assertFullyCovered()
+                methodCounter("org.jetbrains.composetests.ComposeFunctionsKt", "WithParam", "BRANCH").assertFullyCovered()
+
+                methodCounter("org.jetbrains.composetests.ComposeFunctionsKt", "WithDefParam").assertFullyCovered()
+                methodCounter("org.jetbrains.composetests.ComposeFunctionsKt", "WithDefParam", "BRANCH").assertFullyCovered()
+            }
+        }
+    }
+}
---
+++ kover-gradle-plugin/src/functionalTest/templates/builds/android-compose/app/build.gradle.kts
@@ -0,0 +1,78 @@
+/*
+ * Copyright 2017-2024 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license.
+ */
+
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.jetbrains.kotlin.android)
+    id("org.jetbrains.kotlinx.kover")
+}
+
+android {
+    namespace = "org.jetbrains.composetests"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "org.jetbrains.composetests"
+        minSdk = 29
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        vectorDrawables {
+            useSupportLibrary = true
+        }
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+    buildFeatures {
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.5.1"
+    }
+    packaging {
+        resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        }
+    }
+
+    testOptions {
+        unitTests {
+            isIncludeAndroidResources = true
+        }
+    }
+}
+
+dependencies {
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.androidx.activity.compose)
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.ui)
+    implementation(libs.androidx.ui.graphics)
+    implementation(libs.androidx.ui.tooling.preview)
+    implementation(libs.androidx.material3)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+    androidTestImplementation(platform(libs.androidx.compose.bom))
+    androidTestImplementation(libs.androidx.ui.test.junit4)
+    debugImplementation(libs.androidx.ui.tooling)
+    debugImplementation(libs.androidx.ui.test.manifest)
+
+    testImplementation(libs.androidx.ui.test.junit4)
+    testImplementation("org.robolectric:robolectric:4.12.2")
+}
