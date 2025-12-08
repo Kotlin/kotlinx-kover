298bf8b95bf8b3689b665114243de87f763e37ab
Sergey Shanshin <sergey.shanshin@jetbrains.com>
2025-12-08 21:05:52 +0100

    Added support for Android Gradle Plugin 9.0.0
    Fixes #776
    PR #777

---
+++ kover-gradle-plugin/src/functionalTest/templates/builds/android-kmp-library-8/src/androidMain/kotlin/AndroidClass.kt
@@ -0,0 +1,15 @@
+/*
+ * Copyright 2017-2025 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license.
+ */
+
+package org.jetbrains
+
+class AndroidClass {
+    fun a() {
+        println("Hello World!")
+    }
+
+    fun covered() {
+        println("Covered")
+    }
+}
