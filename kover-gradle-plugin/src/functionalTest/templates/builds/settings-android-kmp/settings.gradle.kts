ba341c5e375590e7e140a6caf0366ccd39a7ac28
Sergey.Shanshin <sergey.shanshin@jetbrains.com>
2026-02-10 16:31:54 +0100

    Fixed support of Android KMP libraries
    https://github.com/Kotlin/kotlinx-kover/issues/747#issuecomment-3877821848
    
    Fixes #747

---
+++ kover-gradle-plugin/src/functionalTest/templates/builds/settings-android-kmp/settings.gradle.kts
@@ -0,0 +1,28 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+plugins {
+    id("org.jetbrains.kotlinx.kover.aggregation") version "SNAPSHOT"
+}
+
+enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
+
+rootProject.name = "AndroidKMPLibraryKover"
